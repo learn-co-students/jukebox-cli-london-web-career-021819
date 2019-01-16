d27b773c4817952095aef8973cdc21aebee0fd8b
Calie Rushton <calielrushton@gmail.com>
2019-01-16 10:33:53 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,14 +24,37 @@ def list(songs)
   end
 end
 
-def play(songs)
-
+def play(songs) 
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+  if (1..9).include?(response.to_i)
+    puts "Playing #{songs[response.to_i - 1]}"
+  elsif songs.include?(response)
+    puts "Playing: #{response}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
   puts "Goodbye"
 end
 
-def run
-
+def run(songs)
+  help
+  loop do
+  puts "Please enter a command:"
+  command = gets.chomp
+    case command
+      when "help"
+        help
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      when "exit"
+        exit_jukebox
+        break
+      end
+  end
 end
